cfc848514333f705a9399e5be60f1896e23b6eb2
Veronica Schade <veganbunnie@gmail.com>
2012-03-05 14:55:11 -0500

    Initial Commit

---
+++ VCExample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  VCExample
+//
+//  Created by Veronica Schade on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ VCExampleTests/VCExampleTests.m
@@ -0,0 +1,32 @@
+//
+//  VCExampleTests.m
+//  VCExampleTests
+//
+//  Created by Veronica Schade on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "VCExampleTests.h"
+
+@implementation VCExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in VCExampleTests");
+}
+
+@end
